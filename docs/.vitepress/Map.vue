44ddbc069020c19c2734e021d427453625e25fe8
James Cooper <693888+jameshcooper@users.noreply.github.com>
2025-03-08 10:05:29 +0000

    Brighton, tidies

--- docs/.vitepress/Map.vue
+++ docs/.vitepress/Map.vue
@@ -12,26 +12,21 @@ mapboxgl.accessToken =
 
 const props = defineProps(['map'])
 
-// Define your coordinates with location names as keys
 const locations = {
   noosa: [153.10471635680273, -26.379446409041275],
-  noosa2: [153.10471635680273, -26.379446409041275],
+  brighton: [-0.08301807205713319, 50.861667179460675],
 }
 
-// Get the coordinates based on the map prop
 const getCoords = () => {
-  // If props.map is a string (like 'noosa'), look it up in the locations object
   if (typeof props.map === 'string') {
-    return locations[props.map] || locations['noosa'] // default to noosa if not found
+    return locations[props.map] || locations['noosa']
   }
 
-  // If props.map is a number, use it as an index (keeping backward compatibility)
   if (typeof props.map === 'number') {
-    const arr = [locations.noosa, locations.noosa2]
+    const arr = [locations.noosa, locations.brighton]
     return arr[props.map] || locations.noosa
   }
 
-  // Default
   return locations.noosa
 }
 
@@ -43,7 +38,7 @@ const vTest = {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: coords,
-      zoom: 10,
+      zoom: 14,
     })
   },
 }
